c1975cb09a823272fd9b4be6f0a7be3b25060a93
Miguel Soriano <originalsorianom@gmail.com>
2022-09-05 02:08:33 -0700

    server.js update

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
 const express = require("express");
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 //Setting up server
 const app = express();
 const PORT = process.env.PORT || 2000;
-
+//middleware function
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.json());
-
-app.use(express.static("public"));
-
-app.use()
+app.use(express.static('public'));
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(2000, () => {
     console.log(`API server now on port ${PORT}`);
   });
+  
